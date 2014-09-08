fdd75b2b61cb3a2af1ff2735105f787507050b05
Yu Kitazume <u.kitazume@gmail.com>
2014-09-08 18:46:09 +0900

    add dbjson links

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -3,3 +3,5 @@ sudo "eselect nodejs set 0.10.28"
 sudo "ln -nfs /opt/nodejs/0.10.28 /opt/nodejs/current"
 sudo "ln -nfs /opt/nodejs/current/bin/node /usr/bin/node"
 sudo "npm install cake -g"
+
+run "ln -nfs /data/#{config.app}/shared/config/db.json /data/#{config.app}/current/config/db.json"
